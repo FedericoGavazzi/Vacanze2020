4d30d3ac00889622653b08b2ff483552cad859ed
FedericoGavazzi <56387105+FedericoGavazzi@users.noreply.github.com>
2020-07-03 17:26:53 +0200

    Add files via upload

--- MessaggiCriptati/src/messaggicriptati/Criptatore.java
+++ MessaggiCriptati/src/messaggicriptati/Criptatore.java
@@ -5,6 +5,10 @@
  */
 package messaggicriptati;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 /**
@@ -13,12 +17,26 @@ import java.util.HashMap;
  */
 public class Criptatore {
 
+    /**
+     * chiave di codificazione
+     */
     private static HashMap<Character, Character> chiave = new HashMap<>();
+    /**
+     * chiave di decodificazione
+     */
     private static HashMap<Character, Character> chiaveInversa = new HashMap<>();
+    private static final int LUNGHEZZA_MESSAGGIO = 1000;
 
+    /**
+     * Recupero tutti i caratteri presenti mel messaggio da codificare, ricreo
+     * il messaggio codificato utilizzando la chiave di decriptazione.
+     *
+     * @param messaggio, il messaggio da codificare.
+     * @return il messaggio codificato.
+     */
     public static String cripta(String messaggio) {
         String messaggioCriptato = "";
-        char caratteri[] = new char[400];
+        char caratteri[] = new char[LUNGHEZZA_MESSAGGIO];
         for (int i = 0; i < messaggio.length(); i++) {
             caratteri[i] = messaggio.charAt(i);
         }
@@ -32,9 +50,16 @@ public class Criptatore {
         return messaggioCriptato;
     }
 
+    /**
+     * Recupero tutti i caratteri presenti mel messaggio da decodificare, ricreo
+     * il messaggio decodificato utilizzando la chiave di decriptazione.
+     *
+     * @param messaggio, il messaggio da decodificare.
+     * @return il messaggio decodificato.
+     */
     public static String decripta(String messaggio) {
         String messaggioDecriptato = "";
-        char caratteri[] = new char[400];
+        char caratteri[] = new char[LUNGHEZZA_MESSAGGIO];
         for (int i = 0; i < messaggio.length(); i++) {
             caratteri[i] = messaggio.charAt(i);
         }
@@ -49,111 +74,53 @@ public class Criptatore {
         return messaggioDecriptato;
     }
 
-    public static void inizializzaChiavi() {
-        chiave.put('A', 'h');
-        chiave.put('B', 'f');
-        chiave.put('C', 'G');
-        chiave.put('D', 'I');
-        chiave.put('E', 'M');
-        chiave.put('F', 'O');
-        chiave.put('G', 'b');
-        chiave.put('H', 'l');
-        chiave.put('I', 'e');
-        chiave.put('J', 'd');
-        chiave.put('K', 'N');
-        chiave.put('L', 'o');
-        chiave.put('M', 'F');
-        chiave.put('N', 'c');
-        chiave.put('O', 'i');
-        chiave.put('P', 'A');
-        chiave.put('Q', 'H');
-        chiave.put('R', 'D');
-        chiave.put('S', 'a');
-        chiave.put('T', 'E');
-        chiave.put('U', 'C');
-        chiave.put('V', 'g');
-        chiave.put('W', 'B');
-        chiave.put('X', 'm');
-        chiave.put('Y', 'L');
-        chiave.put('Z', 'n');
-        chiave.put('a', 's');
-        chiave.put('b', 'T');
-        chiave.put('c', 'v');
-        chiave.put('d', 'p');
-        chiave.put('e', 'R');
-        chiave.put('f', 'y');
-        chiave.put('g', 'K');
-        chiave.put('h', 'z');
-        chiave.put('i', 'Q');
-        chiave.put('j', 'P');
-        chiave.put('k', 'J');
-        chiave.put('l', 'r');
-        chiave.put('m', 'V');
-        chiave.put('n', 'w');
-        chiave.put('o', 'S');
-        chiave.put('p', 'j');
-        chiave.put('q', 't');
-        chiave.put('r', 'Z');
-        chiave.put('s', 'k');
-        chiave.put('t', 'W');
-        chiave.put('u', 'x');
-        chiave.put('v', 'Y');
-        chiave.put('w', 'u');
-        chiave.put('x', 'U');
-        chiave.put('y', 'X');
-        chiave.put('z', 'q');
-        chiaveInversa.put('h', 'A');
-        chiaveInversa.put('f', 'B');
-        chiaveInversa.put('G', 'C');
-        chiaveInversa.put('I', 'D');
-        chiaveInversa.put('M', 'E');
-        chiaveInversa.put('O', 'F');
-        chiaveInversa.put('b', 'G');
-        chiaveInversa.put('l', 'H');
-        chiaveInversa.put('e', 'I');
-        chiaveInversa.put('d', 'J');
-        chiaveInversa.put('N', 'K');
-        chiaveInversa.put('o', 'L');
-        chiaveInversa.put('F', 'M');
-        chiaveInversa.put('c', 'N');
-        chiaveInversa.put('i', 'O');
-        chiaveInversa.put('A', 'P');
-        chiaveInversa.put('H', 'Q');
-        chiaveInversa.put('D', 'R');
-        chiaveInversa.put('a', 'S');
-        chiaveInversa.put('E', 'T');
-        chiaveInversa.put('C', 'U');
-        chiaveInversa.put('g', 'V');
-        chiaveInversa.put('B', 'W');
-        chiaveInversa.put('m', 'X');
-        chiaveInversa.put('L', 'Y');
-        chiaveInversa.put('n', 'Z');
-        chiaveInversa.put('s', 'a');
-        chiaveInversa.put('T', 'b');
-        chiaveInversa.put('v', 'c');
-        chiaveInversa.put('p', 'd');
-        chiaveInversa.put('R', 'e');
-        chiaveInversa.put('y', 'f');
-        chiaveInversa.put('K', 'g');
-        chiaveInversa.put('z', 'h');
-        chiaveInversa.put('Q', 'i');
-        chiaveInversa.put('P', 'j');
-        chiaveInversa.put('J', 'k');
-        chiaveInversa.put('r', 'l');
-        chiaveInversa.put('V', 'm');
-        chiaveInversa.put('w', 'n');
-        chiaveInversa.put('S', 'o');
-        chiaveInversa.put('j', 'p');
-        chiaveInversa.put('t', 'q');
-        chiaveInversa.put('Z', 'r');
-        chiaveInversa.put('k', 's');
-        chiaveInversa.put('W', 't');
-        chiaveInversa.put('x', 'u');
-        chiaveInversa.put('Y', 'v');
-        chiaveInversa.put('u', 'w');
-        chiaveInversa.put('U', 'x');
-        chiaveInversa.put('X', 'y');
-        chiaveInversa.put('q', 'z');
+    /**
+     * Inizializzo le mappe per la codifica e decodifica dei messaggi caricando
+     * le informazioni da 4 file. 2 per le informazioni sulla codifica e 2 per
+     * le informazioni sulla decodifica.
+     *
+     * @throws java.io.IOException
+     */
+    public static void inizializzaChiavi() throws IOException {
+        BufferedReader br;
+        //caricamento alfabeto e codifica da file
+        try {
+            br = new BufferedReader(new FileReader("chiaveDiCodifica.txt"));
+            String s;
+            while ((s = br.readLine()) != null) {
+                String caratteri[];
+                caratteri = s.split(",");
+                chiave.put(caratteri[0].charAt(0), caratteri[1].charAt(0));
+            }
+            //caricamento numeri e simboli e codifica da file
+            br = new BufferedReader(new FileReader("altriCaratteriCodifica.txt"));
+            while ((s = br.readLine()) != null) {
+                String caratteri[];
+                caratteri = s.split("a");
+                chiave.put(caratteri[0].charAt(0), caratteri[1].charAt(0));
+            }
+            //caricamento alfabeto e decodifica da file
+            br = new BufferedReader(new FileReader("chiaveDiDecodifica.txt"));
+            while ((s = br.readLine()) != null) {
+                String caratteri[];
+                caratteri = s.split(",");
+                chiaveInversa.put(caratteri[0].charAt(0), caratteri[1].charAt(0));
+            }
+            //caricamento numeri e simboli e decodifica da file
+            br = new BufferedReader(new FileReader("altriCaratteriDecodifica.txt"));
+            while ((s = br.readLine()) != null) {
+                String caratteri[];
+                caratteri = s.split("a");
+                chiaveInversa.put(caratteri[0].charAt(0), caratteri[1].charAt(0));
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Uno o più file non presenti");
+        }
+
+    }
+
+    public static int getLUNGHEZZA_MESSAGGIO() {
+        return LUNGHEZZA_MESSAGGIO;
     }
 
     public static void test() {
--- MessaggiCriptati/src/messaggicriptati/FXMLDocumentController.java
+++ MessaggiCriptati/src/messaggicriptati/FXMLDocumentController.java
@@ -5,8 +5,11 @@
  */
 package messaggicriptati;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,7 +20,7 @@ import javafx.scene.input.KeyEvent;
 
 /**
  *
- * @author feder
+ * @author Federico Gavazzi
  */
 public class FXMLDocumentController implements Initializable {
 
@@ -36,16 +39,29 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Label lunghezzaMessaggioLbl;
 
-    private final int LUNGHEZZA_MASSIMA_MESSAGGIO = 400;
+    private final int LUNGHEZZA_MASSIMA_MESSAGGIO = Criptatore.getLUNGHEZZA_MESSAGGIO();
     @FXML
     private Button pulisciBtn;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         lunghezzaMessaggioLbl.setText("Caratteri restanti: " + LUNGHEZZA_MASSIMA_MESSAGGIO);
-        Criptatore.inizializzaChiavi();
+        try {
+            Criptatore.inizializzaChiavi();
+        } catch (IOException ex) {
+            System.out.println("Problemi con il caricamento delle chiavi");
+        }
+        codificaBtn.setDisable(true);
+        decodificaBtn.setDisable(true);
+        label1.setText("");
     }
 
+    /**
+     * Controllo che il testo non superi la lunghezza massima ogni volta che
+     * viene premuto un tasto della tastiera
+     *
+     * @param event
+     */
     @FXML
     private void controlloLunghezza(KeyEvent event) {
         String testo = InserisciTxt.getText();
@@ -53,25 +69,77 @@ public class FXMLDocumentController implements Initializable {
             lunghezzaMessaggioLbl.setText("Caratteri restanti: " + (LUNGHEZZA_MASSIMA_MESSAGGIO - testo.length()));
         } else {
             InserisciTxt.setDisable(true);
+            lunghezzaMessaggioLbl.setText("Caratteri restanti: " + 0);
+            label1.setText("Verrano codificati/decodificati solo i primi " + LUNGHEZZA_MASSIMA_MESSAGGIO + " caratteri.");
+        }
+
+        if (testo.length() == 0) {
+            codificaBtn.setDisable(true);
+            decodificaBtn.setDisable(true);
+        } else {
+            codificaBtn.setDisable(false);
+            decodificaBtn.setDisable(false);
+            
         }
 
     }
 
+    /**
+     * Quando premo il bottone per codificare prendo il testo dal text field di
+     * inserimento, lo codifico e lo restituisco nel secondo text field, inoltre
+     * viene aggiornata la label in cui si avvisa se il messaggio è stato
+     * codificato e se il campo di testo è vuoto.
+     *
+     * @param event
+     */
     @FXML
     private void codifica(ActionEvent event) {
-        risultatoTxt.setText(Criptatore.cripta(InserisciTxt.getText()));
-
+        if (InserisciTxt.getText().equals("")) {
+            label1.setText("Messaggio vuoto");
+        } else if(InserisciTxt.getText().length() > LUNGHEZZA_MASSIMA_MESSAGGIO){
+            
+            risultatoTxt.setText(Criptatore.cripta(InserisciTxt.getText().substring(0, LUNGHEZZA_MASSIMA_MESSAGGIO-1)));
+            label1.setText("Messaggio codificato");
+        } else {
+            risultatoTxt.setText(Criptatore.cripta(InserisciTxt.getText()));
+            label1.setText("Messaggio codificato");
+        }
     }
 
+    /**
+     * Quando premo il bottone per decodificare prendo il testo dal text field
+     * di inserimento, lo decodifico e lo restituisco nel secondo text field,
+     * inoltre viene aggiornata la label in cui si avvisa se il messaggio è
+     * stato codificato e se il campo di testo è vuoto.
+     *
+     * @param event
+     */
     @FXML
     private void decodifica(ActionEvent event) {
-        risultatoTxt.setText(Criptatore.decripta(InserisciTxt.getText()));
+        if (InserisciTxt.getText().equals("")) {
+            label1.setText("Messaggio vuoto");
+        }else if(InserisciTxt.getText().length() > LUNGHEZZA_MASSIMA_MESSAGGIO){
+            lunghezzaMessaggioLbl.setText("Caratteri restanti: " + 0);
+            risultatoTxt.setText(Criptatore.decripta(InserisciTxt.getText().substring(0, LUNGHEZZA_MASSIMA_MESSAGGIO-1)));
+            label1.setText("Messaggio decodificato");
+        } else {
+            risultatoTxt.setText(Criptatore.decripta(InserisciTxt.getText()));
+            label1.setText("Messaggio decodificato");
+        }
     }
 
+    /**
+     * Pulisco la label e i text field.
+     * Disabilito i pulsanti.
+     * @param event
+     */
     @FXML
     private void pulisci(ActionEvent event) {
         risultatoTxt.clear();
         InserisciTxt.clear();
+        label1.setText("");
+        codificaBtn.setDisable(true);
+        decodificaBtn.setDisable(true);
     }
 
 }
